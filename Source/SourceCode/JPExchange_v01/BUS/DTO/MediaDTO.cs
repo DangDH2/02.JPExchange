728f2e2ebba853cfedddd3cecb6a5d8fb09974b2
DangDH2 <dangdh60256@gmail.com>
2017-09-12 14:18:15 +0900

    A

---
+++ Source/SourceCode/JPExchange_v01/BUS/DTO/MediaDTO.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.DTO
+{
+    public class MediaDTO
+    {
+        public String MediaId { get; set; }
+        public String MediaLink { get; set; }
+        public String MediaType {get; set;}
+        
+        
+        public MediaDTO(){
+        }
+        
+    }
+}
