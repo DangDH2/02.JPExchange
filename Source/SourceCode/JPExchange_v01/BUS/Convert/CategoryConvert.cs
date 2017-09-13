b22c51f3e50c43756c6ee1c3cc49085125e449f5
DangDH2 <dangdh60256@gmail.com>
2017-09-13 21:18:56 +0900

    A

--- Source/SourceCode/JPExchange_v01/BUS/Convert/CategoryConvert.cs
+++ Source/SourceCode/JPExchange_v01/BUS/Convert/CategoryConvert.cs
@@ -1,4 +1,5 @@
 using BUS.DTO;
+using DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,34 @@ using System.Threading.Tasks;
 
 namespace BUS.Convert
 {
-    public class CategoryConvert
+    public static class CategoryConvert
     {
-        
+        public static CATEGORY FromDTOToDB(CategoryDTO dto)
+        {
+            return null;
+        }
+        public static List<CATEGORY> FromDTOToDB(List<CategoryDTO> lsDTO)
+        {
+            return null;
+        }
+        public static CategoryDTO FromDBToDTO(CATEGORY db)
+        {
+            CategoryDTO dto = new CategoryDTO();
+            dto.Id = db.CategoryId.ToString();
+            dto.Name = db.Name;
+
+            return dto;
+        }
+        public static List<CategoryDTO> FromDBToDTO(List<CATEGORY> lsDb)
+        {
+            List<CategoryDTO> ret = new List<CategoryDTO>();
+            foreach (CATEGORY c in lsDb)
+            {
+                ret.Add(FromDBToDTO(c));
+            }
+            return ret;
+        }
+
 
     }
 }
